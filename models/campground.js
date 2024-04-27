30a2f33ea716ebcf997f3c9c6ad4f624205a53c5
Abhishek Arora <arorabhs@amazon.com>
2024-04-27 16:06:12 +0530

    Initial Commit

---
+++ models/campground.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema({
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    price: Number,
+    description: String,
+    location: String,
+    averageRating: { type: Number, min: 0, max: 5, default: 0 },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+}, opts);
+
+CampgroundSchema.index({ title: 'text', location: 'text', description: 'text' });
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
+});
+
+
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+CampgroundSchema.statics.calculateAverageRating = async function(campgroundId) {
+    const pipeline = [
+      {
+        $match: { campground: campgroundId }
+      },
+      {
+        $group: {
+          _id: null,
+          averageRating: { $avg: '$rating' }
+        }
+      }
+    ];
+  
+    const result = await this.model('Review').aggregate(pipeline);
+    return result.length > 0 ? result[0].averageRating : 0;
+  }
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
---
+++ models/review.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    body: String,
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    campground: {
+        type: Schema.Types.ObjectId,
+        ref: 'Campground'
+    },
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
+
